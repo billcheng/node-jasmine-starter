71436faba3aab87e60af20eafe3798c955f62eb2
Bill Cheng <cccheng118@yahoo.com>
2019-06-09 22:39:43 -0500

    1-99 v2

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,16 @@
+const romanSets = [
+    ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
+    ['X','XX','XXX','XL','L','LX','LXX','LXXX','XC']
+];
+
 export const convertToRomanNumerals = (arabic: number) => {
-    return 'I';
+    let result = [];
+    const myRomanSets = [...romanSets];
+    while (arabic > 0) {
+        const romanSet = myRomanSets.shift();
+        const digit = arabic % 10;
+        arabic = Math.floor(arabic / 10);
+        result.unshift(romanSet[digit - 1]);
+    }
+    return result.join('');
 }
